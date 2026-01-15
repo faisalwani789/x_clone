6b1e625e553912d6c4efaabcdd2d4043c9ecd434
Faisal yousuf wani <faisalyousuf1122@gmail.com>
2026-01-15 19:15:13 +0530

    added added auth middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken'
+export const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.headers['authorization']
+    if (!token) return res.send('please login')
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    req.user = decoded
+    // console.log(req.user)
+    next()
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+
+}
