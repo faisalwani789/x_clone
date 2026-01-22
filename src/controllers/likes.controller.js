e98e3017d0d4802e7f0f20a4783d6d26de4283e1
Faisal yousuf wani <faisalyousuf1122@gmail.com>
2026-01-22 22:56:04 +0530

    added likes cnt

---
+++ src/controllers/likes.controller.js
@@ -0,0 +1,23 @@
+import pool from "../config/db.js"
+
+export const addTweetLike=async(req,res)=>{
+    const{id:refId,type}=req.query
+    const{id:userId}=req.user
+    const conn=await pool.getConnection()
+    try {
+        await conn.beginTransaction()
+
+        const[[result]]=await conn.query('call addLike(?,?,?)',[userId,refId,type])
+        console.log(result)
+        
+   
+        res.status(200).json({result })
+        await conn.commit()
+    } catch (error) {
+        await conn.rollback()
+        console.log(error)
+    }
+    finally{
+        conn.release()
+    }
+}
