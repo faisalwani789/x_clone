b59edd677806058a340ece91edd4266567594a32
Faisal yousuf wani <faisalyousuf1122@gmail.com>
2026-01-30 17:44:09 +0530

    added route

--- src/router/tweet.routes.js
+++ src/router/tweet.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { addTweet,getTweets, getFollowingTweets} from "../controllers/tweet.controller.js";
+import { addTweet,getTweets, getFollowingTweets,getTweetById} from "../controllers/tweet.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 const router=Router()
 router.post('/',authMiddleware,upload.array('media',4),addTweet)
 router.get('/',getTweets)
 router.get('/follow',authMiddleware,getFollowingTweets)
-
+router.get('/:id',getTweetById)
 
 
 
