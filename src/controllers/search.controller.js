209df3d2baa1dc421738432e4cbc04f9a124eef8
Faisal yousuf wani <faisalyousuf1122@gmail.com>
2026-02-01 22:03:06 +0530

    added getSearchResultsV2

--- src/controllers/search.controller.js
+++ src/controllers/search.controller.js
@@ -4,7 +4,7 @@ export const getSearchResults = async (req, res) => {
     
     const conn = await pool.getConnection()
     let { query, sortBy = null,direction=null, page, limit } = req.query
-    // console.log(query)
+    console.log(query)
     page = Number(page) || 1
     limit = Number(limit) || 10
     const offset = (page - 1) * limit 
@@ -13,7 +13,7 @@ export const getSearchResults = async (req, res) => {
 
         // const [[result]] = await conn.execute('call getSearchResults(?,?,?,?)', [query, sort, limit, offset])
         const [[result]] = await conn.execute('call getSearchResultsV2(?,?,?,?,?)', [query, sortBy,direction, limit, offset])
-        // console.log(result)
+
         res.status(200).json({ success: true, result })
 
     } catch (error) {
