514dea5aa3f0c5196ecba6b9eda86977771396f4
Faisal yousuf wani <faisalyousuf1122@gmail.com>
2026-02-03 08:44:11 +0530

    added routes

--- src/router/user.routes.js
+++ src/router/user.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { addUser, loginUser } from "../controllers/user.controller.js";
+import { addUser, loginUser, sendOtp, verifyEmail } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 const router=Router()
 router.post('/',upload.fields([{name:'profile',maxCount:1},{name:'cover',maxCount:1}]),addUser)
 router.post('/login',loginUser)
+router.post('/send-otp',sendOtp)
+router.post('/verify-email',verifyEmail)
 
 export default router
