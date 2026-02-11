bc92d57ef7abb69d37adb52f3bea791e8294664c
Faisal yousuf wani <faisalyousuf1122@gmail.com>
2026-02-11 19:38:52 +0530

    added readNotification

--- src/controllers/notificaion.controller.js
+++ src/controllers/notificaion.controller.js
@@ -17,3 +17,32 @@ export const getNotifications=async(req,res)=>{
         conn.release
     }
 }
+
+export const readNotification=async(req,res)=>{
+    const conn=await pool.getConnection()
+    const{id}=req.user
+    const{notificationId}=req.params
+    const query=`update notifications set isRead = ? where id = ? and targetUserId = ?`
+    try {
+        await conn.beginTransaction()
+        const[notifcation]=await conn.execute(query,[true,notificationId,id])
+        await conn.commit()
+        if (notifcation.affectedRows===0){
+            return res.status(400).json({
+            success:false,
+            message:'notification not found'
+        })
+        }
+        console.log(notifcation)
+        res.status(200).json({
+            success:true,
+            message:'notification read successsfully'
+        })
+    } catch (error) {
+        await conn.rollback()
+        console.log(error)
+    }
+    finally{
+        conn.release
+    }
+}
--- src/router/notification.routes.js
+++ src/router/notification.routes.js
@@ -1,9 +1,9 @@
-import { getNotifications } from "../controllers/notificaion.controller.js";
+import { getNotifications, readNotification } from "../controllers/notificaion.controller.js";
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router=Router()
 router.get('/',authMiddleware,getNotifications)
-
+router.patch('/read/:notificationId',authMiddleware,readNotification)
 
 export default router
